3de174c5508081741a8d880bfee7f00caaab45bc
Ogum <86077359+ogumanagonye@users.noreply.github.com>
2022-05-11 23:53:56 +0000

    Add files via upload

---
+++ Aula1/Prob2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+
+    int a;
+
+    printf("Insira um inteiro: \n");
+    scanf("%d",&a);
+
+    printf("O numero inserido é %d \n", a);
+}
---
+++ Aula1/Prob4.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main(){
+    printf("Inicio do programa \n Meio do Programa \n Fim do programa. \n");
+}
